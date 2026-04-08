0d837ed01f2cc6d2a2bf5ffa0ffbee0c9dbb4b28
devYuno <annykessy20@gmail,com>
2026-04-07 21:57:09 -0300

    Credenciais adicionadas

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import './App.css';
 
+ const credenciais = {
+    emailReal: 'pucpr@gmail.com',
+    senhaReal: '12345'
+  }
+
 class App extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
       email: '',
-      senha: ''
+      senha: '',
+      message: ''
     }
   }
 
   setEmail(event) {
     let email = event.target.value
-    this.setState({ email: email})
+    this.setState({ email: email })
   }
 
   setSenha(event) {
     let senha = event.target.value
-    this.setState({ senha: senha})
+    this.setState({ senha: senha })
   }
 
-  render () {
+  render() {
     return (
       <div className='page'>
         <h2>Login</h2>
-        <input id='input_email' name='email' type='text' onChange={(e) => {this.setEmail(e)}}></input>
-        <input id='input_senha' name='senha' type='text' onChange={(e) => {this.setSenha(e)}}></input>
+        <input id='input_email' name='email' type='text' onChange={(e) => { this.setEmail(e) }}></input>
+        <input id='input_senha' name='senha' type='text' onChange={(e) => { this.setSenha(e) }}></input>
         <button>Acessar</button>
+        <span>Acesso liberado</span>
       </div>
     )
   }
