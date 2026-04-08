bebf5a4409e8c64d6c14537047a865f3978915fc
devYuno <annykessy20@gmail,com>
2026-04-07 21:37:48 -0300

    Inputs adicionados

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,10 +2,33 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      senha: ''
+    }
+  }
+
+  setEmail(event) {
+    let email = event.target.value
+    this.setState({ email: email})
+  }
+
+  setSenha(event) {
+    let senha = event.target.value
+    this.setState({ senha: senha})
+  }
+
   render () {
     return (
-      <>
-      </>
+      <div className='page'>
+        <h2>Login</h2>
+        <input id='input_email' name='email' type='text' onChange={(e) => {this.setEmail(e)}}></input>
+        <input id='input_senha' name='senha' type='text' onChange={(e) => {this.setSenha(e)}}></input>
+        <h3>{this.state.email}</h3>
+        <h3>{this.state.senha}</h3>
+      </div>
     )
   }
 }
