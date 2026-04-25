fb518de960172f6e413bff4a9813c8267e9ba93f
devYuno <annykessy20@gmail,com>
2026-04-25 14:51:18 -0300

    FEAT: automatizando os testes unitarios

--- web-app/src/routes/AppRoutes.js
+++ web-app/src/routes/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from '../pages/Login/login';
 import Register from '../pages/Register/register';
 import Home from '../pages/Home/home';
@@ -7,13 +7,13 @@ import Home from '../pages/Home/home';
 class AppRoutes extends React.Component {
     render() {
         return (
-            <HashRouter basename='/WebReact_PUC'>
+            <BrowserRouter basename='/WebReact_PUC'>
                 <Routes>
                     <Route path="/" element={<Login/>}></Route>
                     <Route path="/register" element={<Register/>}></Route>
                     <Route path="/home" element={<Home/>}></Route>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         )
     }
 }
